25ac00219b78316b3cfffef4141f7edf1514d221
scarsty <scarsty@gmail.com>
2026-07-29 15:05:23 +0800

    补充资源

--- make7z.bat
+++ make7z.bat
@@ -1,4 +1,3 @@
 call cp.bat
 del kys-cpp-4in1.7z
-copy ..\x64\release\kys.exe bin\
-7za a -mx9 -t7z kys-cpp-4in1.7z ./bin ./game ./game-dragon ./game-ptt ./game-RiverLake play.bat cpplauncher.exe games.ini
+7za a -mx9 -t7z kys-cpp-4in1.7z ./bin ./licenses ./game ./game-dragon ./game-ptt ./game-RiverLake play.bat cpplauncher.exe games.ini
--- make7z2x.bat
+++ make7z2x.bat
@@ -1,4 +1,3 @@
 call cp.bat
 del kys-cpp-2x.7z
-copy ..\x64\release\kys.exe bin\
-7za a kys-cpp-2x.7z ./bin ./game-dragon play-dragon.bat game.ini cpplauncher.exe
+7za a kys-cpp-2x.7z ./bin ./licenses ./game-dragon play-dragon.bat games.ini cpplauncher.exe
