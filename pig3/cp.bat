acc14536229d1d098fd5303c150a022e6962d832
scarsty <scarsty@gmail.com>
2026-07-31 15:18:29 +0800

    缩进

--- pig3/cp.bat
+++ pig3/cp.bat
@@ -3,4 +3,4 @@ copy ..\..\..\_pascal\pig3\kys-pig3\game\script\event\*.lua game\script\event\
 copy ..\..\..\_pascal\pig3\kys-pig3\game0\script\event\*.lua game0\script\event\
 copy ..\..\..\_pascal\pig3\kys-pig3\game\script\event-cifa\*.cifa game\script\event-cifa\
 copy ..\..\..\_pascal\pig3\kys-pig3\game0\script\event-cifa\*.cifa game0\script\event-cifa\
-pause
+timeout /t 60 /nobreak
